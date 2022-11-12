7c38c5ec36d549ce8c4ce2f400d1a4fc8ed89831
Tomáš Macháček <piroot01@gmail.com>
2022-11-12 19:56:40 +0100

    lib added

---
+++ lib/include/cpp_serial.h
@@ -0,0 +1,16 @@
+#ifndef CPP_SERIAL_H
+#define CPP_SERIAL_H
+
+// System headers
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <asm/ioctl.h>
+#include <asm/termbits.h>
+
+// User headers
+
+#include "exception.h"
+
+#endif
