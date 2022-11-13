b68a6d3b5852a58fffc3d6505b56516d86c748f2
Tomáš Macháček <piroot01@gmail.com>
2022-11-13 12:56:42 +0100

    test of serial communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,48 @@
+//
+// main.cpp
+//
+
+// system headers
+#include <chrono>
+#include <thread>
+#include <string>
+
+// user headers
 #include "../lib/include/cpp_serial.hpp"
+
+#define WAIT_FOR_SERIAL_INIT 2000
+
+using namespace CppSerial;
 
 int
 main()
 {
+    char myMessage[2] = {'i', '\r'};
+    std::string readBuffer;
+
+    // Set the parameters for serial communication.
+    SerialPort mySerial("/dev/ttyACM0", BaudRate::B_9600, NumDataBits::EIGHT, Parity::NONE, NumStopBits::ONE);
+    
+    // Set timeout.
+    mySerial.SetTimeout(-1);
+    
+    // Open serial port.
+    mySerial.Open();
+
+    // Wait for 2 seconds to establish serial communication.
+    std::this_thread::sleep_for(std::chrono::milliseconds(WAIT_FOR_SERIAL_INIT));
+    
+    // Send my message.
+    mySerial.Write(myMessage);
+    
+    // Read the answer.
+    mySerial.Read(readBuffer);
+
+    // Print readBuffer on screen.
+    std::cout << readBuffer << std::endl;
+
+    // Close serial port.
+    mySerial.Close();
+
     return 0;
 }
