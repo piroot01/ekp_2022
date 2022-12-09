c6b66fcbaaf4535d9e70833323d9cd919b4774dd
Tomáš Macháček <piroot01@gmail.com>
2022-12-09 19:01:45 +0100

    utils: string to int optimization

--- src/utils/char_to_int.cpp
+++ src/utils/char_to_int.cpp
@@ -1,17 +1,19 @@
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <stdio.h>
 
 int main() {
     std::string inp;
     
     std::cin >> inp;
 
-    int nthNum = 0, numSize = 0;
-    int* tmpNum = NULL;
+    //int nthNum = 0, numSize = 0;
+    //int* tmpNum = NULL;
     unsigned int num = 0;
-
+/*
     for (char& tmpChar : inp) {
         if (nthNum == numSize) {
             (nthNum == 0)?(numSize = 1):(numSize *= 2);
@@ -24,10 +26,13 @@ int main() {
     for (int i = 0; i < nthNum; ++i) {
         num += std::pow(10, i) * tmpNum[nthNum - i - 1];
     }
+*/
+
+    num = std::stoi(inp);
 
     std::cout << num << std::endl;
 
-    free(tmpNum);
+    //free(tmpNum);
     exit(0);
 }
 
