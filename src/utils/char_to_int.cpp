9e6629c0622a7021316f6a1cd20abc20e834cea2
Tomáš Macháček <piroot01@gmail.com>
2022-12-09 13:31:14 +0100

    utils: string to int

--- src/utils/char_to_int.cpp
+++ src/utils/char_to_int.cpp
@@ -1,12 +1,33 @@
+#include <cstdlib>
 #include <iostream>
 #include <string>
+#include <cmath>
 
 int main() {
     std::string inp;
     
     std::cin >> inp;
-    std::cout << inp << std::endl;
 
+    int nthNum = 0, numSize = 0;
+    int* tmpNum = NULL;
+    unsigned int num = 0;
+
+    for (char& tmpChar : inp) {
+        if (nthNum == numSize) {
+            (nthNum == 0)?(numSize = 1):(numSize *= 2);
+            tmpNum = (int*)realloc(tmpNum, numSize * sizeof(int));
+        }
+        tmpNum[nthNum] = (int)tmpChar - 48;
+        nthNum++;
+    }
+
+    for (int i = 0; i < nthNum; ++i) {
+        num += std::pow(10, i) * tmpNum[nthNum - i - 1];
+    }
+
+    std::cout << num << std::endl;
+
+    free(tmpNum);
     exit(0);
 }
 
